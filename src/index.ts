e1ef4c873fcc660dd1205c7ec8b2c6bff2a043e2
HARITJOSHI1 <joshiharit3@gmail.com>
2024-06-14 18:41:31 +0530

    init commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express, { type Response } from "express";
+import winston from "winston";
+
+const app = express();
+app.use(express.json());
+
+// Winston logger setup
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "load-balancer.log" }),
+  ],
+});
+
+app.get("/", (req, res) => {
+  console.log("in here");
+  res.status(200).json({ message: "Working" });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`);
+});
