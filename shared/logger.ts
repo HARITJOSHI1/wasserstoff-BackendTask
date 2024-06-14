16a8a176d8ee3e5e6ebb6e8ee5f0d7eda57ed4f4
HARITJOSHI1 <joshiharit3@gmail.com>
2024-06-14 21:05:18 +0530

    mc-3

--- shared/logger.ts
+++ shared/logger.ts
@@ -1,12 +1,15 @@
-import winston from "winston";
+// import winston from "winston";
 
-export default function LoggerModule() {
-  return winston.createLogger({
-    level: "info",
-    format: winston.format.json(),
-    transports: [
-      new winston.transports.Console(),
-      new winston.transports.File({ filename: "load-balancer.log" }),
-    ],
-  });
-}
+// export default function LoggerModule() {
+//   return winston.createLogger({
+//     level: "info",
+//     format: winston.format.json(),
+//     transports: [
+//       new winston.transports.Console(),
+//       new winston.transports.File({ filename: "load-balancer.log" }),
+//     ],
+//   });
+// }
+
+
+export default () => {}
