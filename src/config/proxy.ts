fc1b7867ed1e4b216d14c9a2030ea4bf4db54baa
HARITJOSHI1 <joshiharit3@gmail.com>
2024-06-14 21:54:09 +0530

    mc-11

--- src/config/proxy.ts
+++ src/config/proxy.ts
@@ -1,6 +1,5 @@
 import type { NextFunction, Response, Request } from "express";
 import proxy from "express-http-proxy";
-import { LOAD_BALANCER_PORT } from "..";
 
 const validPorts = [4001, 4002, 4003];
 const proxyMiddleware = (req: Request, res: Response, next: NextFunction) => {
@@ -14,7 +13,7 @@ const proxyMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const portUrl = portMatch && parseInt(portMatch[1], 10);
   const port = parseInt(req.headers["x-port"] as string, 10);
 
-  if (!port && portUrl === LOAD_BALANCER_PORT) return next();
+  if (!port && portUrl === +process.env.PORT!) return next();
 
   if (validPorts.includes(port)) {
     return proxy(`http://localhost:${port}`)(req, res, next);
