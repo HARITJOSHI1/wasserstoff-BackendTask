29c6682fcd862f73ee08563cb28460defb61a642
HARITJOSHI1 <joshiharit3@gmail.com>
2024-06-27 14:10:49 +0530

    cleanup

--- api/index.ts
+++ api/index.ts
@@ -1,24 +1,17 @@
 import express from "express";
-import LoggerModule from "../shared/logger.js";
-import proxyMiddleware from "./config/proxy.js";
 
-const LOAD_BALANCER_PORT = process.env.PORT || 4000;
+const APP_PORT = process.env.PORT || 4000;
 const app = express();
-const logger = LoggerModule();
 
 app.use(express.json());
 
-app.use("/demo", (_, res) =>
-  res.status(200).json({ message: `Working ${LOAD_BALANCER_PORT}` })
-);
+app.use("/demo", (_, res) => {
+  return res.json({message: "It works!!!!"})
+});
 
-// app.use("/demo", (req, _, next) => {
-//   req.headers["x-port"] = "4001";
-//   return next();
-// }, proxyMiddleware);
-
-// app.all("*", (_, res) => res.status(404).json({ message: "Route not found" }));
+app.all("*", (_, res) => res.status(404).json({ message: "Route not found" }));
 
 app.listen(4000, () => {
-  logger.info(`Server running on LOAD_BALANCER_PORT ${LOAD_BALANCER_PORT}`);
+  console.info(`Server running on PORT ${APP_PORT}`);
+  
 });
